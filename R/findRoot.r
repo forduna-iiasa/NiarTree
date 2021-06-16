3db5fb755a127296a72333c0a06903edf94e2130
forduna-iiasa <46443076+forduna-iiasa@users.noreply.github.com>
2021-06-16 20:16:28 +0200

    Delete findRoot.r

--- R/findRoot.r
+++
@@ -1,9 +0,0 @@
-source("DEuclidean.r")
-findRoot <- function(data,att){
-  g <- data[,att]
-  dataord<- order(data[,att])
-  data <- data[dataord,]
-  med <- mean(data[,att])
-  caso <- MinEuclidean1xn(med,data[,att])
-  return(caso)
-}
